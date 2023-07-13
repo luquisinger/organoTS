852c64a546a93c91b331cfaf7d521d17cbad9d7f
Matheus Luchsinger <102822195+luquisinger@users.noreply.github.com>
2023-07-13 16:34:42 -0300

    1

---
+++ src/componentes/Banner/index.tsx
@@ -0,0 +1,21 @@
+import './Banner.css'
+
+
+
+interface BannerProps {
+    enderecoImagem: string
+    textoAlternativo?: string
+}
+
+export const Banner = ({ enderecoImagem, textoAlternativo } :BannerProps) => {
+    // JSX
+    return (
+        <header className="banner">
+            {/* <img src="" alt="O banner principal da página do Organo"/> */}
+            <img src={enderecoImagem} alt={textoAlternativo}/>
+
+        </header>
+    )
+}
+
+export default Banner
